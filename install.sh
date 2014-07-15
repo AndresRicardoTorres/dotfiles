f24d55585d2fab62107ba78985ac369f414a2d17
Andrés Ricardo Torres Martínez <torresandresricardo@gmail.com>
2014-07-15 11:56:49 -0500

    Plugins de vim

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ cp git/gitconfig ~/.gitconfig
 
 # Vim
 mkdir -p ~/.vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vim/vimrc ~/.vimrc
 cp -rf vim/colors ~/.vim/
 
