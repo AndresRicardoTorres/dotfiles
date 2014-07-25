a29635d64ba31c9ce743baeb7e1e8b750971bd60
Andrés Ricardo Torres Martínez <torresandresricardo@gmail.com>
2014-07-25 18:26:57 -0500

    Ignorar advertencias en install, mejoras para vim

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# Instala o actualiza los archivos de configuracion
+
 # Awesome
 mkdir -p ~/.config/awesome/
 cp awesome/rc.lua ~/.config/awesome/rc.lua
@@ -7,7 +10,9 @@ cp git/gitconfig ~/.gitconfig
 
 # Vim
 mkdir -p ~/.vim
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d ~/.vim/bundle/ ]; then
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 cp vim/vimrc ~/.vimrc
 cp -rf vim/colors ~/.vim/
 
@@ -18,11 +23,15 @@ cp xinit/xinitrc ~/.xinitrc
 cp yaourt/yaourtrc ~/.yaourtrc
 
 # Zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+if [ ! -d ~/.oh-my-zsh/ ]; then 
+  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
 
 cp zsh/erikasv.zsh-theme ~/.oh-my-zsh/themes/
 cp zsh/zshrc_mine ~/.zshrc
 cp zsh/zkeybindings ~/.zkeybindings
 cp zsh/zshrc_oh-my-zsh  ~/.zshrc_oh-my-zsh 
 
-chsh -s /bin/zsh
+if [ ! "$SHELL" = "/bin/zsh" ]; then
+  chsh -s /bin/zsh
+fi
